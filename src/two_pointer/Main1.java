d27109d2c9dd5ce4bf20c67796e22c6fdcfec9d7
여지원 <yooara0508@naver.com>
2022-01-02 12:52:23 +0900

    two pointer, sliding window 1, 2, 3, 4번 sol

---
+++ src/two_pointer/Main1.java
@@ -0,0 +1,37 @@
+package two_pointer;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+        Integer[] array1 = Arrays.stream(in.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+        int m = Integer.parseInt(in.readLine());
+        Integer[] array2 = Arrays.stream(in.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+
+        new Main1().solution(n, array1, m, array2);
+    }
+
+    public void solution(int n, Integer[] array1, int m, Integer[] array2) {
+        int[] result = new int[n + m];
+        int idx = 0;
+        int dn = 0, dm = 0;
+
+        while (dn < n && dm < m) {
+            if (array1[dn] < array2[dm])
+                result[idx++] = array1[dn++];
+            else
+                result[idx++] = array2[dm++];
+        }
+
+        while (dn < n)
+            result[idx++] = array1[dn++];
+
+        while (dm < m)
+            result[idx++] = array2[dm++];
+
+        Arrays.stream(result).forEach(r -> System.out.print(r + " "));
+    }
+}
--- src/two_pointer/Main4.java
+++ src/two_pointer/Main4.java
@@ -1,17 +1,37 @@
+package two_pointer;
+
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main4 {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         Integer[] nm = Arrays.stream(in.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
         Integer[] nums = Arrays.stream(in.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
 
-        new Main().solution(nm[0], nm[1], nums);
+        new Main4().solution(nm[0], nm[1], nums);
     }
 
-
     public void solution(Integer n, Integer m, Integer[] nums) {
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            for (int j = i+1; j < n; j++) {
+                sum += nums[j];
+                if (sum > m) break;
+                if (sum == m) {
+                    count++;
+                    break;
+                }
+            }
+            sum = 0;
+        }
+        System.out.println(count);
+    }
+
+    public void solution2(Integer n, Integer m, Integer[] nums) {
         int count = 0;
         int sum = nums[0];
         int lt = 0, rt = 1;
