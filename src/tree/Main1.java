406d20e2569b53c72a2b2a5c3db768f34401271e
여지원 <yooara0508@naver.com>
2022-01-03 12:41:25 +0900

    two pointer, sliding window 5, 6번, map 1번 sol two pointer, sliding window 끝

--- src/tree/Main1.java
+++ src/tree/Main1.java
@@ -1,16 +1,40 @@
+package tree;
+
 import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(in.readLine());
         List<String> votes = Arrays.stream(in.readLine().split("")).collect(Collectors.toList());
-        new Main().solution(votes, n);
+        new Main1().solution(votes, n);
     }
 
     public void solution(List<String> votes, int n) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String vote : votes) {
+            if(!map.containsKey(vote))
+                map.put(vote, 1);
+            else{
+                map.put(vote, map.get(vote) + 1);
+            }
+        }
+
+        Map.Entry<String, Integer> max = null;
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if(max == null || entry.getValue().compareTo(max.getValue())>0){
+                max = entry;
+            }
+        }
+
+        System.out.println(max.getKey());
+    }
+
+    public void solution2(List<String> votes, int n) {
         Map<String, Integer> map = new HashMap<>();
 
         for (String vote : votes)
