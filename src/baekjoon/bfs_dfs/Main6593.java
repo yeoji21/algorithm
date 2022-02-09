113590e913fac938bbe227ca0595af0dc9add0c2
여지원 <yooara0508@naver.com>
2022-02-09 12:49:27 +0900

    백준 6593번

--- src/baekjoon/bfs_dfs/Main6593.java
+++ src/baekjoon/bfs_dfs/Main6593.java
@@ -1,8 +1,10 @@
+package baekjoon.bfs_dfs;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class Main6593 {
     static char[][][] building;
     static int[][][] checked;
     static int[] dl = {1, -1, 0, 0, 0, 0};
