edfc03cdfe8c54e843e255824e38636fc71ee967
여지원 <yooara0508@naver.com>
2022-02-16 14:15:46 +0900

    백준 17141번

--- src/baekjoon/bfs_dfs/Main17141.java
+++ src/baekjoon/bfs_dfs/Main17141.java
@@ -1,27 +1,26 @@
-import java.awt.*;
+package baekjoon.bfs_dfs;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.List;
 
-public class Main {
+public class Main17141 {
     static int N, M;
     static int[][] map;
     static List<Point> viruses = new ArrayList<>();
-    static Point[] selectedViruses;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
+    static Point[] selectedLocation;
     static int result = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][N];
-        selectedViruses = new Point[M];
 
+        selectedLocation = new Point[M];
+        map = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
@@ -31,19 +30,14 @@ public class Main {
             }
         }
 
-        if(isFull(map)) {
-            System.out.println("0");
-            return;
-        }
-
-        spreadVirus(0, 0);
+        getVirusLocation(0, 0);
 
         if(result == Integer.MAX_VALUE)
             System.out.println(-1);
         else System.out.println(result);
     }
 
-    private static void spreadVirus(int count, int level) {
+    private static void getVirusLocation(int count, int level) {
         if (count == M) {
             int[][] copy = Arrays.stream(map).map(int[]::clone).toArray(int[][]::new);
             BFS(copy);
@@ -51,39 +45,32 @@ public class Main {
         }
         for (int i = level; i < viruses.size(); i++) {
             Point point = viruses.get(i);
-            selectedViruses[count] = point;
+            selectedLocation[count] = point;
             map[point.x][point.y] = 3;
-            spreadVirus(count + 1, i+1);
+            getVirusLocation(count + 1, i+1);
             map[point.x][point.y] = 2;
         }
     }
 
     private static void BFS(int[][] copy) {
         Queue<Point> queue = new LinkedList<>();
+        queue.addAll(List.of(selectedLocation));
         int count = 0;
-        boolean[][] checked = new boolean[N][N];
-        Arrays.stream(selectedViruses).forEach(x -> {
-            queue.add(x);
-            checked[x.x][x.y] = true;
-        });
 
         while (!queue.isEmpty()) {
             int size = queue.size();
             count++;
             for (int i = 0; i < size; i++) {
                 Point poll = queue.poll();
+
                 for (int j = 0; j < 4; j++) {
                     int nx = poll.x + dx[j];
                     int ny = poll.y + dy[j];
 
-                    if (nx < 0 || ny < 0 || nx >= N || ny >= N ||
-                            copy[nx][ny] == 1 || checked[nx][ny])
-                        continue;
+                    if (nx < 0 || ny < 0 || nx >= N || ny >= N || copy[nx][ny] == 1 || copy[nx][ny] == 3) continue;
 
-                    if(copy[nx][ny] == 2 && !isActivate(copy, nx, ny)) continue;
-                    queue.add(new Point(nx, ny));
                     copy[nx][ny] = 3;
-                    checked[nx][ny] = true;
+                    queue.add(new Point(nx, ny));
                 }
             }
         }
@@ -93,25 +80,17 @@ public class Main {
     }
 
     private static boolean isFull(int[][] copy) {
-        return Arrays.stream(copy).flatMapToInt(Arrays::stream).allMatch(x -> x!=0);
-    }
-
-    private static boolean isActivate(int[][] copy, int x, int y) {
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-            if(copy[nx][ny] == 0 || copy[nx][ny] == 2) return true;
-        }
-        return false;
+        return Arrays.stream(copy).flatMapToInt(Arrays::stream).allMatch(x -> x == 3 || x == 1);
     }
 
 
     static class Point{
         int x, y;
+
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
+
     }
 }
