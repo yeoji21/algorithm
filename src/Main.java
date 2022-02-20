4d0caa834dbcd95f6dce73848187860fd707f4d6
여지원 <yooara0508@naver.com>
2022-02-20 12:40:34 +0900

    백준 1726번

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
@@ -9,143 +8,98 @@ public class Main {
     static int N, M;
     static int[][] map;
     static Point start, dest;
+    static boolean[][][] checked;
+    static int[] dx = {0, 0, 0, 1, -1};
+    static int[] dy = {0, 1, -1, 0, 0};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
 
-        for (int i = 0; i < N; i++) {
+        map = new int[N+1][M+1];
+        checked = new boolean[N + 1][M + 1][5];
+
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for (int j = 1; j <= M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         st = new StringTokenizer(br.readLine());
-        start = new Point(Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken()));
+        start = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()), 0);
         st = new StringTokenizer(br.readLine());
-        dest = new Point(Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken()));
-
+        dest = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()), 0);
         BFS();
     }
 
     private static void BFS() {
-        boolean[][][] checked = new boolean[N][M][5];
-        int[] dx = {0, 0, 0, 1, -1};
-        int[] dy = {0, 1, -1, 0, 0};
         Queue<Point> queue = new LinkedList<>();
-        queue.add(start);
         checked[start.x][start.y][start.d] = true;
-        int time = 0;
+        queue.offer(start);
 
         while (!queue.isEmpty()) {
-            time++;
-            int size = queue.size();
-//            System.out.println(size);
-//            System.out.println("************************");
-//            queue.stream().forEach(System.out::println);
+            Point now = queue.poll();
+            int x = now.x;
+            int y = now.y;
+            int dir = now.d;
+            int count = now.count;
 
-            while (size-- > 0) {
-                Point now = queue.poll();
-//                System.out.println(now);
+            if (isAnswer(x, y, dir)) {
+                System.out.println(count);
+                return;
+            }
 
-                if(now.x == dest.x && now.y == dest.y && now.d == dest.d) {
-                    System.out.println(time);
-                    return;
-                }
+            for (int i = 1; i <= 3; i++) {
+                int nx = x + dx[dir] * i;
+                int ny = y + dy[dir] * i;
 
-//                if(now.x == dest.x && now.y == dest.y){
-//                    if(now.d == dest.d){
-//                        System.out.println(time);
-//                        return;
-//                    }
-//                    else{
-////                        queue.stream().forEach(System.out::println);
-//                        time++;
-//                        directionCheck(time, now, dest.d);
-//                        System.out.println(time);
-//                        return;
-//                    }
-//                }
+                if(rangeCheck(nx, ny) || map[nx][ny] == 1) break;
+                if(checked[nx][ny][dir]) continue;
 
-                for (int i = 1; i < 5; i++) {
-                    int nx = now.x + dx[i];
-                    int ny = now.y + dy[i];
+                checked[nx][ny][dir] = true;
+                queue.offer(new Point(nx, ny, dir, count + 1));
+            }
 
-                    if(rangeCheck(nx, ny)) continue;
-                    if(checked[nx][ny][i] || map[nx][ny] == 1) continue;
-
-                    if (now.d == i) {
-                        sameDirection(checked, queue, now);
-                    }else{
-                        directionCheck(time, now, i);
-                        queue.offer(new Point(now.x, now.y, i));
-                        checked[now.x][now.y][i] = true;
-                    }
+            for (int i = 1; i <= 4; i++) {
+                if(i != dir && !checked[x][y][i]) {
+                    queue.offer(new Point(x, y, i, count + directionCheck(dir, i)));
+                    checked[x][y][i] = true;
                 }
             }
         }
-
-
     }
 
-    private static void directionCheck(int time, Point now, int i) {
-        if(now.d == 1 && i == 2) time++;
-        else if(now.d == 2 && i == 1) time++;
-        else if(now.d == 3 && i == 4) time++;
-        else if(now.d == 4 && i == 3) time++;
+    private static boolean isAnswer(int x, int y, int d) {
+        return x == dest.x && y == dest.y && d == dest.d;
     }
 
-    private static void sameDirection(boolean[][][] checked, Queue<Point> queue, Point now) {
-        for (int w = 1; w < 4; w++) {
-            Point go = go(now, w);
-            if(go == null) break;
-            if(map[go.x][go.y] != 1 && !checked[go.x][go.y][go.d]){
-                queue.offer(go);
-                checked[go.x][go.y][go.d] = true;
-            }
-        }
-    }
-
-    private static Point go(Point now, int w) {
-        Point temp = new Point(now.x, now.y, now.d);
-        if(temp.d == 1){
-            temp.y += w;
-        } else if (temp.d == 2) {
-            temp.y -= w;
-        }else if (temp.d == 3) {
-            temp.x += w;
-        }else if (temp.d == 4) {
-            temp.x -= w;
-        }
-        if(!rangeCheck(temp.x, temp.y))
-            return temp;
-        return null;
+    private static int directionCheck(int now, int next) {
+        int result = 1;
+        if(now == 1 && next == 2) result = 2;
+        else if(now == 2 && next == 1) result = 2;
+        else if(now == 3 && next == 4) result = 2;
+        else if(now == 4 && next == 3) result = 2;
+        return result;
     }
 
     private static boolean rangeCheck(int nx, int ny) {
-        return nx < 0 || ny < 0 || nx >= N || ny >= M;
+        return nx <= 0 || ny <= 0 || nx > N || ny > M;
     }
 
     static class Point{
-        int x, y, d;
+        int x, y, d, count;
 
-        public Point(int x, int y, int d) {
+        public Point(int x, int y, int d, int count) {
             this.x = x;
             this.y = y;
             this.d = d;
-        }
-
-        @Override
-        public String toString() {
-            return "Point{" +
-                    "x=" + x +
-                    ", y=" + y +
-                    ", d=" + d +
-                    '}';
+            this.count = count;
         }
     }
 }
