7cb65571b38000d64611730b2115a317e4127e7a
여지원 <yooara0508@naver.com>
2022-02-26 16:44:42 +0900

    백준 14503번

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,55 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int[] dx = {-1, 0, 1, 0};
-    private static int[] dy = {0, 1, 0, -1};
-    private static int[][] map;
-    private static int N, M;
-    private static Point robot;
+    static int[][] map = new int[101][101];
+    static int R, C, K;
+    static final int LIMIT = 100;
+    static int result = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = getNextIntToken(st);
-        M = getNextIntToken(st);
+        R = getIntegerToken(st);
+        C = getIntegerToken(st);
+        K = getIntegerToken(st);
 
-        map = new int[N][M];
-        st = new StringTokenizer(br.readLine());
-        robot = new Point(getNextIntToken(st), getNextIntToken(st), getNextIntToken(st));
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i < 4; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                map[i][j] = getNextIntToken(st);
+            for (int j = 1; j < 4; j++) {
+                map[i][j] = getIntegerToken(st);
             }
         }
 
-        clean();
+        sort();
+        // https://bcp0109.tistory.com/216
+        // https://velog.io/@hyunjkluz/%EB%B0%B1%EC%A4%8017140-%EC%9D%B4%EC%B0%A8%EC%9B%90-%EB%B0%B0%EC%97%B4%EA%B3%BC-%EC%97%B0%EC%82%B0-Java
+        // https://comgong-man.tistory.com/235
     }
 
-    private static void clean() {
+    private static void sort() {
+        while (result++ < LIMIT) {
+            int row = map.length;
+            int column = map[0].length;
 
-        map[robot.x][robot.y] = 1;
-        int leftDirection = getLeftDirection(robot.d);
+            if(row >= column) RSort();
+            else CSort();
 
-    }
-
-    private static int getLeftDirection(int now) {
-        if(now == 0) return 3;
-        else if(now == 1) return 0;
-        else if(now == 2) return 1;
-        else if(now == 3) return 2;
-        return -1;
-    }
-
-    private static class Point{
-        int x, y, d;
-
-        public Point(int x, int y, int d) {
-            this.x = x;
-            this.y = y;
-            this.d = d;
+            if(map[R][C] == K) return;
         }
     }
 
-    private static int getNextIntToken(StringTokenizer st) {
+    private static void RSort() {
+
+    }
+
+    private static void CSort() {
+
+    }
+
+    private static int getIntegerToken(StringTokenizer st) {
         return Integer.parseInt(st.nextToken());
     }
 }
