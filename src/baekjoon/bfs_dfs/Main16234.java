79c45a83e50454639ef7351b4f40c4c894c0de4a
여지원 <yooara0508@naver.com>
2022-02-28 12:53:43 +0900

    백준 16234번

--- src/baekjoon/bfs_dfs/Main16234.java
+++ src/baekjoon/bfs_dfs/Main16234.java
@@ -1,8 +1,10 @@
+package baekjoon.bfs_dfs;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class Main16234 {
     static int N, L, R;
     static int[][] map;
     static int[][] check;
