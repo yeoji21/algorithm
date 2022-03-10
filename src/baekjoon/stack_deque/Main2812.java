6c473bad94ef20128f4fbe4902913c0df3a8275b
여지원 <yooara0508@naver.com>
2022-03-10 12:23:30 +0900

    백준 2812번

--- src/baekjoon/stack_deque/Main2812.java
+++ src/baekjoon/stack_deque/Main2812.java
@@ -1,21 +1,34 @@
+package baekjoon.stack_deque;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
-public class Main {
+public class Main2812 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+        int count = 0;
 
         Integer[] target = Arrays.stream(br.readLine().split("")).map(Integer::parseInt).toArray(Integer[]::new);
         Stack<Integer> stack = new Stack<>();
 
+        for (int i = 0; i < target.length; i++) {
+            while (count < K && !stack.isEmpty() && stack.peek() < target[i]) {
+                stack.pop();
+                count++;
+            }
+            stack.push(target[i]);
+        }
 
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N-K; i++) {
+            sb.append(stack.get(i));
+        }
+        System.out.println(sb.toString());
     }
 }
