e97fa89a66c21133870309a21c5271f6bc9839b6
여지원 <yooara0508@naver.com>
2022-03-17 12:49:51 +0900

    ### 3. Two Pointer - 4번. 연속 부분수열 - 6번. 최대 길이 연속부분수열
    ### 4. HashMap, TreeSet
    - 4번. 모든 아나그램 찾기

--- src/inflean/hashmap_set/Main4_1.java
+++ src/inflean/hashmap_set/Main4_1.java
@@ -1,9 +1,11 @@
+package inflean.hashmap_set;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Main {
+public class Main4_1 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String S = br.readLine();
