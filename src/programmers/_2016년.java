b21363cdbf4358c1d9d255e562153155f596ad12
여지원 <yooara0508@naver.com>
2022-06-15 23:09:28 +0900

    프로그래머스 문제 2016년

---
+++ src/programmers/_2016년.java
@@ -0,0 +1,9 @@
+package programmers;
+
+import java.time.LocalDate;
+
+public class _2016년 {
+    public String solution(int a, int b) {
+        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
+    }
+}
