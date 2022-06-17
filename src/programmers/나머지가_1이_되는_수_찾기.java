e5a112071f2da7de103c5bf0d092d03465289655
여지원 <yooara0508@naver.com>
2022-06-17 23:27:44 +0900

    프로그래머스 문제

---
+++ src/programmers/나머지가_1이_되는_수_찾기.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class 나머지가_1이_되는_수_찾기 {
+    public int solution(int n) {
+        int i = 0;
+        for (i = 2; i <= Math.sqrt(n-1); i++) {
+            if((n - 1) % i == 0) return i;
+        }
+
+        return n-1;
+    }
+}
