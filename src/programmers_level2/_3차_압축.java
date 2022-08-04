ff9182bf547bcac713c3b4daace7c20e092e5e03
여지원 <yooara0508@naver.com>
2022-08-04 14:57:53 +0900

    프로그래머스 2단계 알고리즘

--- src/programmers_level2/_3차_압축.java
+++ src/programmers_level2/_3차_압축.java
@@ -1,22 +1,46 @@
 package programmers_level2;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class _3차_압축 {
-    public int[] solution(String msg) {
+
+    static int idx = 1;
+    public static int[] solution(String msg) {
         Map<String, Integer> dictionary = initailizeDictionary();
-        int idx = 27;
+        List<Integer> result = new ArrayList<>();
 
+        Queue<Character> queue = new LinkedList<>();
+        for (int i = 0; i < msg.length(); i++) {
+            queue.add(msg.charAt(i));
+        }
 
+        while (!queue.isEmpty()) {
+            Character now = queue.poll();
+            StringBuilder sb = new StringBuilder();
+            sb.append(now);
+            while(dictionary.containsKey(sb.toString())){
+                if(dictionary.containsKey(sb.toString() + queue.peek())){
+                    sb.append(queue.peek());
+                    queue.poll();
+                }
+                else{
+                    break;
+                }
+            }
+            result.add(dictionary.get(sb.toString()));
+            if (!queue.isEmpty()) {
+                sb.append(queue.peek());
+                dictionary.put(sb.toString(), idx++);
+            }
+        }
 
-        return new int[]{};
+        return result.stream().mapToInt(x -> x).toArray();
     }
 
-    private Map<String, Integer> initailizeDictionary() {
+    private static Map<String, Integer> initailizeDictionary() {
         Map<String, Integer> dictionary = new HashMap<>();
 
-        int idx = 1;
+        idx = 1;
         for (char c = 'A'; c <= 'Z'; c++) {
             dictionary.put(String.valueOf(c), idx++);
         }
---
+++ src/programmers_level2/_3차_파일명_정렬.java
@@ -0,0 +1,45 @@
+package programmers_level2;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+// TODO: 2022/08/04 다른 사람들 풀이 보기
+public class _3차_파일명_정렬 {
+    public static String[] solution(String[] files) {
+        List<File> processedFile = new ArrayList<>(files.length * 2);
+
+        for (int i = 0; i < files.length; i++) {
+            String file = files[i];
+            String original = file;
+            String head = file.split("\\d+")[0];
+            file = file.replace(head, "");
+            String number = file.split("\\D+")[0];
+            File newFile = new File(original, head, number, i);
+            processedFile.add(newFile);
+        }
+
+        return processedFile.stream()
+                .sorted(
+                        Comparator.comparing((File file) -> file.head)
+                                .thenComparing((File file) -> file.number)
+                                .thenComparing((File file) -> file.idx)
+                )
+                .map(file -> file.originalName)
+                .toArray(String[]::new);
+    }
+
+    static class File{
+        String originalName;
+        String head;
+        int number;
+        int idx;
+
+        public File(String originalName, String head, String number, int idx) {
+            this.originalName = originalName;
+            this.head = head.toLowerCase();
+            this.number = Integer.parseInt(number);
+            this.idx = idx;
+        }
+    }
+}
