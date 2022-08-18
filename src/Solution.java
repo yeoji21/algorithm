e2aa17f7402965adfd99d18b38dd61a63fdb82f9
여지원 <yooara0508@naver.com>
2022-08-18 12:35:08 +0900

    프로그래머스 2단계 알고리즘

--- src/Solution.java
+++ src/Solution.java
@@ -1,56 +1,58 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Solution {
     public static void main(String[] args) {
+        int solution = solution(2, new String[]{"N~F=0", "R~T>2"});
+        System.out.println(solution);
+    }
 
+    private static char[] members = new char[]{'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+    private static int result = 0;
+    public static int solution(int n, String[] data) {
+        char[] selected = new char[8];
+        DFS(0, data, selected, new boolean[8]);
+        return result;
+    }
+
+    private static void DFS(int L, String[] data, char[] selected, boolean[] checked) {
+        if (L == 8) {
+            if(validate(data, selected))
+                result++;
+            return;
+        }
+
+        for (int i = 0; i < members.length; i++) {
+            if(checked[i]) continue;
+            checked[i] = true;
+            selected[L] = members[i];
+            DFS(L + 1, data, selected, checked);
+            checked[i] = false;
+        }
 
     }
 
-    public String[] solution(String[] record) {
-        List<Record> result = new ArrayList<>();
-        Map<String, String> nicknames = new HashMap<>();
+    private static boolean validate(String[] data, char[] selected) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < selected.length; i++) sb.append(selected[i]);
+        String select = sb.toString();
 
-        for (String row : record) {
-            String[] split = row.split(" ");
-            String command = split[0];
+        for (String condition : data) {
+            int indexA = select.indexOf(condition.charAt(0));
+            int indexB = select.indexOf(condition.charAt(2));
 
-            if (command.equals("Enter")) {
-                String uuid = split[1];
-                String nickname = split[2];
-                nicknames.put(uuid, nickname);
-                result.add(new Record(command, uuid));
-            } else if (command.equals("Leave")) {
-                result.add(new Record(command, split[1]));
-            } else if (command.equals("Change")) {
-                String uuid = split[1];
-                String nickname = split[2];
-                nicknames.put(uuid, nickname);
+            char op = condition.charAt(3);
+            int gap = condition.charAt(4) - '0';
+            if (op == '=') {
+                if(Math.abs(indexA - indexB) != gap + 1)
+                    return false;
+            } else if (op == '>') {
+                if(Math.abs(indexA - indexB) <= gap + 1)
+                    return false;
+//                if(!(Math.abs(indexA - indexB) > gap+1)) return false;
+            } else if (op == '<') {
+                if(Math.abs(indexA - indexB) >= gap + 1)
+                    return false;
+//                if(!(Math.abs(indexA - indexB) < gap+1)) return false;
             }
         }
-
-        return result.stream()
-                .map(r -> {
-                    String nickname = nicknames.get(r.uuid);
-                    if (r.command.equals("Enter")) {
-                        return nickname + "님이 들어왔습니다.";
-                    } else if (r.command.equals("Leave")) {
-                        return nickname + "님이 나갔습니다.";
-                    }
-                    return null;
-                })
-                .toArray(String[]::new);
-    }
-
-    static class Record{
-        String command;
-        String uuid;
-
-        public Record(String command, String uuid) {
-            this.command = command;
-            this.uuid = uuid;
-        }
+        return true;
     }
 }
