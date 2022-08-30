e531c9e523053edfcf8ba433564a9a9d791ceec2
여지원 <yooara0508@naver.com>
2022-08-30 23:45:37 +0900

    프로그래머스 2단계 알고리즘

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,40 @@
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class Solution {
-    public String solution(int[] numbers) {
-        String answer = Arrays.stream(numbers)
-                .mapToObj(String::valueOf)
-                .sorted((o1, o2) -> (o2 + o1).compareTo(o1 + o2))
-                .collect(Collectors.joining());
-        if(answer.charAt(0) == '0') return "0";
+    public static void main(String[] args) {
+        int solution = solution("JAN");
+        System.out.println(solution);
+    }
+    private static int answer = Integer.MAX_VALUE;
+    public static int solution(String name) {
+        boolean[] checked = new boolean[name.length()];
+        changeAlphabet(name, checked, 0, 0);
         return answer;
+    }
+
+    private static void changeAlphabet(String target, boolean[] checked, int now, int count) {
+        if(now > target.length() - 1 || now < 0) return;
+
+        if(checked[now]) return;
+        if(target.charAt(now) == 'A'){
+            changeAlphabet(target, Arrays.copyOf(checked, checked.length), now + 1, count);
+            changeAlphabet(target, Arrays.copyOf(checked, checked.length), now - 1, count);
+        }
+        char to = target.charAt(now);
+        int min = Math.min(Math.abs('A' - to), Math.abs(to - 'Z') + 1);
+        checked[now] = true;
+        count += min;
+        if(over(checked))
+            answer = Math.min(answer, count);
+
+        changeAlphabet(target, Arrays.copyOf(checked, checked.length), now + 1, count + 1);
+        changeAlphabet(target, Arrays.copyOf(checked, checked.length), now - 1, count + 1);
+    }
+
+    private static boolean over(boolean[] checked) {
+        for (int i = 0; i < checked.length; i++) {
+            if(!checked[i]) return false;
+        }
+        return true;
     }
 }
--- src/programmers_level2/소수_찾기.java
+++ src/programmers_level2/소수_찾기.java
@@ -1,60 +1,37 @@
 package programmers_level2;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class 소수_찾기 {
-    static Set<Integer> set = new HashSet<>();
-
+    private Set<Integer> set = new HashSet<>();
     public int solution(String numbers) {
-        int[] nums = Arrays.stream(numbers.split(""))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        boolean[] checked = new boolean[numbers.length()];
-        recursion(nums, checked, "", 0);
+        String[] splited = numbers.split("");
+        permutation(splited, new boolean[splited.length], "");
         return set.size();
     }
 
-    private void recursion(int[] nums, boolean[] checked, String sb, int L) {
-        if (!sb.equals("")){
-            int target = Integer.parseInt(sb);
-            if(isPrime(target))
-                set.add(target);
-        }
-        if(L == nums.length) return;
-        for (int i = 0; i < nums.length; i++) {
+    private void permutation(String[] numbers, boolean[] checked, String selected) {
+        if(selected.length() > numbers.length) return;
+
+        if(!selected.isBlank() && isPrime(selected))
+            set.add(Integer.valueOf(selected));
+
+        for (int i = 0; i < numbers.length; i++) {
             if(checked[i]) continue;
             checked[i] = true;
-            recursion(nums, checked, sb + nums[i], L+1);
+            permutation(numbers, checked, selected + numbers[i]);
             checked[i] = false;
         }
     }
 
-//    static void dfs(String str, String temp, int m) {
-//        if(temp.length() == m){
-//            int num = Integer.parseInt(temp);
-//            if(!arr.contains(num)){
-//                arr.add(num);
-//            }
-//        }
-//
-//        for(int i=0; i<str.length(); i++){
-//            if(!check[i]){
-//                check[i] = true;
-//                temp += str.charAt(i);
-//                dfs(str, temp, m);
-//                check[i] = false;
-//                temp = temp.substring(0, temp.length()-1);
-//            }
-//        }
-//    }
+    private boolean isPrime(String num) {
+        int number = Integer.parseInt(num);
+        if(number == 0 || number == 1) return false;
 
-    public boolean isPrime(int num){
-        if (num == 1 || num == 0) return false;
-        for (int i = 2; i <= Math.sqrt(num); i++)
-            if (num % i == 0) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if(number % i == 0) return false;
+        }
         return true;
     }
 }
