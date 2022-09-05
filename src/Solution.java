a05b2b09539bea74c72b300d34d488ff9afea859
여지원 <yooara0508@naver.com>
2022-09-05 17:08:35 +0900

    프로그래머스 2단계 알고리즘

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,17 @@
-public class Solution {
+import java.util.*;
 
+public class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String[] clothe : clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
+        }
+
+        int result = 1;
+        for (Integer value : map.values()) {
+            result *= value + 1;
+        }
+        return result - 1;
+    }
 }
--- src/programmers_level2/위장.java
+++ src/programmers_level2/위장.java
@@ -1,29 +1,26 @@
 package programmers_level2;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 public class 위장 {
     public int solution(String[][] clothes) {
-//        Map<String, Integer> map = new HashMap<>();
-//
-//        for (String[] clothe : clothes) {
-//            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
-//        }
-//
-//        int result = 1;
-//
-//        for (Integer value : map.values()) {
-//            result *= value + 1;
-//        }
-//
-//        return result - 1;
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] clothe : clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
+        }
 
-        return Arrays.stream(clothes)
-                .collect(Collectors.groupingBy(c -> c[1], Collectors.mapping(c -> c[0], Collectors.counting())))
-                .values()
-                .stream()
-                .reduce(1L, (x, y) -> x * (y + 1))
-                .intValue() - 1;
+        int result = 1;
+        for (Integer value : map.values()) {
+            result *= value + 1;
+        }
+        return result - 1;
+
+//        return Arrays.stream(clothes)
+//                .collect(Collectors.groupingBy(c -> c[1], Collectors.mapping(c -> c[0], Collectors.counting())))
+//                .values()
+//                .stream()
+//                .reduce(1L, (x, y) -> x * (y + 1))
+//                .intValue() - 1;
     }
 }
