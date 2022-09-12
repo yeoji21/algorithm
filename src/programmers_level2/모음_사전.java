0d65139292bd9beb28e3f5a55b77edbb1dd2de15
여지원 <yooara0508@naver.com>
2022-09-12 12:22:55 +0900

    프로그래머스 2단계 알고리즘

--- src/programmers_level2/모음_사전.java
+++ src/programmers_level2/모음_사전.java
@@ -1,27 +1,16 @@
 package programmers_level2;
 
 public class 모음_사전 {
-    public static void main(String[] args) {
-        int solution = solution("AAAE");
-        System.out.println(solution);
-    }
-
-    public static int solution(String word) {
-        char[] vowels = new char[]{'A', 'E', 'I', 'O', 'U'};
-        int[] pastWords = new int[]{781, 156, 31, 6, 1};
-
-        int result = 0;
+    public int solution(String word) {
+        String str = "AEIOU";
+        int[] gap = {781, 156, 31, 6, 1};
+        int answer = word.length();
 
         for (int i = 0; i < word.length(); i++) {
-            for (int j = 0; j < vowels.length; j++) {
-                if(word.charAt(i) == vowels[j]) {
-                    result += pastWords[i] * j + 1;
-                    break;
-                }
-            }
+            int idx = str.indexOf(word.charAt(i));
+            answer += gap[i] * idx;
         }
 
-        return result;
+        return answer;
     }
-
 }
--- src/programmers_level2/전력망을_둘로_나누기.java
+++ src/programmers_level2/전력망을_둘로_나누기.java
@@ -1,43 +1,47 @@
 package programmers_level2;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class 전력망을_둘로_나누기 {
-    public static void main(String[] args) {
-        solution(4, new int[][]{{1, 2}, {2, 3}, {3, 4}});
-    }
-
-    public static int solution(int n, int[][] wires) {
-        int result = Integer.MAX_VALUE;
+    public int solution(int n, int[][] wires) {
+        int answer = Integer.MAX_VALUE;
 
         for (int i = 0; i < wires.length; i++) {
-            int[] check = initialize(n);
-            Map<Integer, Integer> map = new HashMap<>();
-
-            for (int j = 0; j < wires.length; j++) {
-                if(i == j) continue;
-                int[] wire = wires[j];
-                int min = Math.min(check[wire[0]], check[wire[1]]);
-                check[wire[0]] = min;
-                check[wire[1]] = min;
-                map.put(min, map.getOrDefault(min, 0) + 1);
-            }
-
-            if(map.values().size() == 2) {
-                result = Math.min(result, map.values().stream().reduce((x, y) -> Math.abs(x - y)).get());
-                if(result == 0) break;
-            }
+            int gap = splitGrid(wires, i, n);
+            answer = Math.min(answer, gap);
         }
 
-        return result;
+        return answer;
     }
 
-    private static int[] initialize(int n) {
-        int[] check = new int[n + 1];
-        for (int j = 1; j < check.length; j++) {
-            check[j] = j;
+    private int splitGrid(int[][] wires, int excludeIdx, int n) {
+        int[] union = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            union[i] = i;
         }
-        return check;
+
+        for (int i = 0; i < wires.length; i++) {
+            if(i == excludeIdx) continue;
+            int[] wire = wires[i];
+            union(union, wire[0], wire[1]);
+        }
+
+        int one = 0;
+        for (int j = 0; j < n + 1; j++) {
+            if(find(union, j) == 1) one++;
+        }
+        return Math.abs(n - 2 * one);
+    }
+
+    private void union(int[] union, int x, int y) {
+        int vx = find(union, x);
+        int vy = find(union, y);
+
+        if(vx == vy) return;
+        if(vx < vy) union[vy] = vx;
+        else union[vx] = vy;
+    }
+
+    private int find(int[] union, int idx) {
+        if(union[idx] == idx) return idx;
+        return union[idx] = find(union, union[idx]);
     }
 }
