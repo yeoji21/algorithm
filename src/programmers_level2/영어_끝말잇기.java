57eb15fb30c77ce744292f03871434439c477a44
여지원 <yooara0508@naver.com>
2022-09-24 18:01:39 +0900

    프로그래머스 2단계 알고리즘

--- src/programmers_level2/영어_끝말잇기.java
+++ src/programmers_level2/영어_끝말잇기.java
@@ -2,28 +2,21 @@ package programmers_level2;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Stack;
 
 public class 영어_끝말잇기 {
-    public static int[] solution(int n, String[] words) {
-        int[] result = new int[2];
-        Set<String> check = new HashSet<>();
-        Stack<String> stack = new Stack<>();
-
-
-        for (int round = 0; round < words.length / n + 1; round++) {
-            for (int i = 0; i < n && round * n + i < words.length; i++) {
-                String word = words[round * n + i];
-                if ((!stack.isEmpty() && stack.peek().charAt(stack.peek().length() - 1) != word.charAt(0))
-                        || check.contains(word)) {
-                    result[0] = i + 1;
-                    result[1] = round + 1;
-                    return result;
-                }
-                stack.add(word);
-                check.add(word);
+    public int[] solution(int n, String[] words) {
+        Set<String> set = new HashSet<>();
+        set.add(words[0]);
+        for (int i = 1; i < words.length; i++) {
+            String word = words[i];
+            if (!word.startsWith(String.valueOf(words[i - 1].charAt(words[i - 1].length() - 1))) || set.contains(word)) {
+                int times = i / n + 1;
+                int who = i % n + 1;
+                return new int[]{who, times};
             }
+            set.add(word);
         }
+
 
         return new int[]{0, 0};
     }
--- src/programmers_level2/프렌즈_4블록.java
+++ src/programmers_level2/프렌즈_4블록.java
@@ -1,91 +1,77 @@
 package programmers_level2;
 
 public class 프렌즈_4블록 {
-    public static void main(String[] args) {
-        int solution = solution(4, 5, new String[]{"CCBDE", "AAADE", "AAABF", "CCBBF"});
-        System.out.println(solution);
-    }
+    private static int WIDTH, HEIGHT;
+    private static final int[] dx = {0, -1, -1, 0};
+    private static final int[] dy = {0, -1, 0, -1};
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        WIDTH = n;
+        HEIGHT = m;
 
-    static char[][] map;
-    public static int solution(int m, int n, String[] board) {
-        int result = 0;
-
-        map = new char[m][n];
-        for (int i = 0; i < m; i++) {
+        char[][] map = new char[WIDTH][HEIGHT];
+        for (int i = 0; i < board.length; i++) {
             map[i] = board[i].toCharArray();
         }
 
-        while (true) {
-            boolean[][] remove = checkRemove(m, n);
-
-            int removeCount = remove(remove);
-            if(removeCount == 0) break;
-            result += removeCount;
-            fillDown(m, n);
+        while(true){
+            boolean[][] checked = getRemoveBlocks(map);
+            int count = remove(checked, map);
+            if(count == 0) break;
+            answer += count;
         }
 
-        return result;
+        return answer;
     }
 
-    private static void fillDown(int x, int y) {
-        for (int i = x - 1; i >= 0; i--) {
-            for (int j = 0; j < y; j++) {
-                if(map[i][j] != '.') continue;
+    private boolean[][] getRemoveBlocks(char[][] map) {
+        boolean[][] checked = new boolean[WIDTH][HEIGHT];
+        for (int i = 1; i < WIDTH; i++) {
+            for (int j = 1; j < HEIGHT; j++) {
+                char character = map[i][j];
+                if(character == '.') continue;
+                boolean flag = true;
 
-                int k = i;
-                while(k > 0 && map[k][j] == '.') k--;
-                if(map[k][j] == '.') continue;
-                map[i][j] = map[k][j];
-                map[k][j] = '.';
+                for (int d = 0; d < 4; d++) {
+                    if (map[i + dx[d]][j + dy[d]] != character) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(!flag) continue;
+
+                for (int d = 0; d < 4; d++) {
+                    checked[i + dx[d]][j + dy[d]] = true;
+                }
             }
         }
+        return checked;
     }
 
-    private static int remove(boolean[][] remove) {
+    private int remove(boolean[][] checked, char[][] map) {
         int count = 0;
-
-        for (int i = 0; i < remove.length; i++) {
-            for (int j = 0; j < remove[i].length; j++) {
-                if (remove[i][j]) {
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                if(checked[i][j]) {
                     map[i][j] = '.';
                     count++;
                 }
             }
         }
 
-        return count;
-    }
+        for (int x = HEIGHT - 1; x >= 0; x--) {
+            for (int y = 0; y < WIDTH; y++) {
+                if(map[x][y] != '.') continue;
 
-    static int[] dx = {0, 0, 1, 1};
-    static int[] dy = {0, 1, 0, 1};
-    private static boolean[][] checkRemove(int x, int y) {
-        boolean[][] checked = new boolean[x][y];
+                int idx = x;
+                while (idx > 0 && map[idx][y] == '.') idx--;
 
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-                char target = map[i][j];
-                if(target == '.') continue;
-
-                boolean flag = true;
-                for (int d = 1; d < 4; d++) {
-                    int nx = i + dx[d];
-                    int ny = j + dy[d];
-
-                    if (nx >= x || ny >= y || target != map[nx][ny]) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if(!flag) continue;
-                for (int d = 0; d < 4; d++) {
-                    int nx = i + dx[d];
-                    int ny = j + dy[d];
-                    checked[nx][ny] = true;
-                }
+                if(map[idx][y] == '.') continue;
+                map[x][y] = map[idx][y];
+                map[idx][y] = '.';
             }
         }
 
-        return checked;
+        return count;
     }
-
 }
