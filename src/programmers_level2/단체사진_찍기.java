e8e9061457624871094aaec6dc42613495b2a625
여지원 <yooara0508@naver.com>
2022-10-02 13:13:33 +0900

    프로그래머스 2단계 알고리즘

--- src/programmers_level2/단체사진_찍기.java
+++ src/programmers_level2/단체사진_찍기.java
@@ -1,57 +1,49 @@
 package programmers_level2;
 
 public class 단체사진_찍기 {
-    static char[] people = new char[]{'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
-    // count를 static으로 쓰니까 틀림
-    private int count = 0;
-    static char[] selected = new char[8];
+    private int answer = 0;
+    private char[] characters = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
 
     public int solution(int n, String[] data) {
-        DFS(0, new boolean[8], data);
-        return count;
+        permutation(data, new boolean[characters.length], "", 0);
+        return answer;
     }
 
-    private void DFS(int L, boolean[] visited, String[] data) {
-        if (L == 8) {
-            if(check(selected.clone(), data))
-                count++;
-        }else{
-            for (int i = 0; i < 8; i++) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    selected[L] = people[i];
-                    DFS(L + 1, visited, data);
-                    visited[i] = false;
-                }
-            }
+    private void permutation(String[] conditions, boolean[] checked, String selected, int L) {
+        if (L == characters.length) {
+            if(checkConditions(conditions, selected))
+                answer++;
+            return;
+        }
+
+        for (int i = 0; i < characters.length; i++) {
+            if(checked[i]) continue;
+            checked[i] = true;
+            permutation(conditions, checked, selected + characters[i], L + 1);
+            checked[i] = false;
         }
     }
 
-    private boolean check(char[] selected, String[] data) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < selected.length; i++) {
-            sb.append(selected[i]);
-        }
-        String select = sb.toString();
+    private boolean checkConditions(String[] conditions, String selected) {
+        for (String condition : conditions) {
+            char command = condition.charAt(3);
+            int size = condition.charAt(4) - '0';
+            int indexA = selected.indexOf(condition.charAt(0));
+            int indexB = selected.indexOf(condition.charAt(2));
 
-        for (String condition : data) {
-            int xIdx = select.indexOf(condition.charAt(0));
-            int yIdx = select.indexOf(condition.charAt(2));
-
-            int gap = condition.charAt(4) - '0';
-            char op = condition.charAt(3);
-
-            if(op == '='){
-                if(Math.abs(xIdx - yIdx) != gap+1) return false;
+            if (command == '=') {
+                if (Math.abs(indexA - indexB) - 1 != size)
+                    return false;
             }
-            else if(op == '>'){
-                if(Math.abs(xIdx - yIdx) <= gap+1) return false;
+            if (command == '<') {
+                if (Math.abs(indexA - indexB) >= size + 1)
+                    return false;
             }
-            else if(op == '<'){
-                if(Math.abs(xIdx - yIdx) >= gap+1) return false;
+            if (command == '>'){
+                if(Math.abs(indexA - indexB) <= size + 1)
+                    return false;
             }
         }
         return true;
     }
-
 }
--- src/programmers_level2/문자열_압축.java
+++ src/programmers_level2/문자열_압축.java
@@ -1,48 +1,7 @@
 package programmers_level2;
 
-import java.util.Stack;
-
 public class 문자열_압축 {
     public int solution(String s) {
-        if(s.length() == 1) return 1;
-
-        int minLength = Integer.MAX_VALUE;
-        for (int i = 1; i <= s.length() / 2; i++) {
-            String[] split = s.split("(?<=\\G.{" + i + "})");
-            int result = encrypt(split);
-            minLength = Math.min(result, minLength);
-        }
-
-        return minLength;
-    }
-
-    private int encrypt(String[] split) {
-        Stack<String> stack = new Stack<>();
-        int result = 0;
-
-        for (int i = 0; i < split.length; i++) {
-            if(stack.isEmpty()) stack.push(split[i]);
-            else {
-                if (stack.peek().equals(split[i])) stack.push(split[i]);
-                else {
-                    if(stack.size() == 1) result += stack.peek().length();
-                    else result += stack.peek().length() + String.valueOf(stack.size()).length();
-
-                    stack.clear();
-                    stack.push(split[i]);
-                }
-            }
-        }
-
-        if(!stack.isEmpty()) {
-            if(stack.size() == 1) result += stack.peek().length();
-            else result += stack.peek().length() + String.valueOf(stack.size()).length();
-        }
-
-        return result;
-    }
-
-    public int solution2(String s) {
         int result = s.length();
 
         for (int i = 1; i <= s.length() / 2; i++) {
@@ -73,5 +32,32 @@ public class 문자열_압축 {
         }
 
         return result;
+    }
+
+    public int solution2(String s) {
+        int answer = s.length();
+        for (int gap = 1; gap < s.length() / 2 + 1; gap++) {
+            String before = s.substring(0, gap);
+            int startIdx = gap;
+            int result = 0;
+            int count = 1;
+            while (startIdx <= s.length()) {
+                int endIndex = startIdx + gap;
+                if(endIndex > s.length()) endIndex = s.length();
+                String substring = s.substring(startIdx, endIndex);
+                if(before.equals(substring))
+                    count++;
+                else{
+                    result += gap;
+                    result += count == 1 ? 0 : String.valueOf(count).length();
+                    count = 1;
+                    before = substring;
+                }
+                startIdx += gap;
+            }
+            if(before.length() != gap) result += before.length();
+            answer = Math.min(answer, result);
+        }
+        return answer;
     }
 }
