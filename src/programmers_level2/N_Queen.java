61c241ca7c9ccd31aab7fb3094eda178fe75b7f1
여지원 <yooara0508@naver.com>
2022-10-11 18:34:15 +0900

    프로그래머스 2단계 알고리즘

--- src/programmers_level2/N_Queen.java
+++ src/programmers_level2/N_Queen.java
@@ -1,39 +1,34 @@
 package programmers_level2;
 
 public class N_Queen {
-    // https://excited-hyun.tistory.com/189
-    // https://velog.io/@hyeon930/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-N-Queen-Java
-    // https://yeongjin13.tistory.com/62
     private int answer = 0;
     public int solution(int n) {
         for (int i = 0; i < n; i++) {
-            int[] col = new int[n];
-            col[0] = i;
-            backtracking(col, 1);
+            int[] dp = new int[n];
+            dp[0] = i;
+            backtracking(dp, 1);
         }
+
         return answer;
     }
 
-    private void backtracking(int[] col, int row) {
-        if (row == col.length) {
+    private void backtracking(int[] dp, int level) {
+        if (level == dp.length) {
             answer++;
             return;
         }
-
-        for (int i = 0; i < col.length; i++) {
-            col[row] = i;
-            if(check(col, row)){
-                backtracking(col, row + 1);
-            }else{
-                col[row] = 0;
-            }
+        for (int i = 0; i < dp.length; i++) {
+            if(i == dp[level - 1]) continue;
+            dp[level] = i;
+            if (!check(dp, level)) continue;
+            backtracking(dp, level + 1);
         }
     }
 
-    private boolean check(int[] col, int row) {
-        for (int i = 0; i < row; i++) {
-            if(col[i] == col[row]) return false;
-            if(Math.abs(col[i] - col[row]) == Math.abs(i - row)) return false;
+    private boolean check(int[] dp, int level) {
+        for (int i = 0; i < level; i++) {
+            if(dp[i] == dp[level]) return false;
+            if(Math.abs(level - i) - Math.abs(dp[level] - dp[i]) == 0) return false;
         }
         return true;
     }
