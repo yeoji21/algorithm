f1ea8c2ef89946f1745ca284dac34d9ae82d069e
여지원 <yooara0508@naver.com>
2022-10-13 17:48:04 +0900

    프로그래머스 2단계 알고리즘

--- src/programmers_level2/가장_큰_정사각형_찾기.java
+++ src/programmers_level2/가장_큰_정사각형_찾기.java
@@ -1,36 +1,26 @@
 package programmers_level2;
 
 public class 가장_큰_정사각형_찾기 {
+    private int[] dx = {-1, -1, 0};
+    private int[] dy = {-1, 0, -1};
     public int solution(int [][]board) {
-        int answer = 0;
-        int[][] dp = new int[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            dp[i][0] = board[i][0];
-            answer = Math.max(answer, dp[i][0]);
-        }
-        for (int i = 0; i < board[0].length; i++) {
-            dp[0][i] = board[0][i];
-            answer = Math.max(answer, dp[0][i]);
-        }
-
-        int[] dx = new int[]{-1, -1, 0};
-        int[] dy = new int[]{-1, 0, -1};
-
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 1; j < dp[0].length; j++) {
-                if(board[i][j] != 1) continue;
-
-                int min = 1000;
+        for (int i = 1; i < board.length; i++) {
+            for (int j = 1; j < board[0].length; j++) {
+                if(board[i][j] == 0) continue;
+                int min = Integer.MAX_VALUE;
                 for (int d = 0; d < 3; d++) {
-                    int nx = i + dx[d];
-                    int ny = j + dy[d];
-                    min = Math.min(dp[nx][ny], min);
+                    min = Math.min(min, board[i + dx[d]][j + dy[d]]);
                 }
-                dp[i][j] = min + 1;
-                answer = Math.max(min + 1, answer);
+                board[i][j] = min + 1;
             }
         }
 
-        return answer * answer;
+        int max = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                max = Math.max(max, board[i][j]);
+            }
+        }
+        return max * max;
     }
 }
--- src/programmers_level2/다음_큰_숫자.java
+++ src/programmers_level2/다음_큰_숫자.java
@@ -3,10 +3,10 @@ package programmers_level2;
 public class 다음_큰_숫자 {
     public int solution(int n) {
         int count = Integer.bitCount(n);
-
-        while(true){
-            if(Integer.bitCount(++n) == count)
+        while (++n <= 1_000_000) {
+            if(Integer.bitCount(n) == count)
                 return n;
         }
+        return 0;
     }
 }
