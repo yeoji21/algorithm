ab27c97ab905ac3287b1c3f271af7892339857db
여지원 <yooara0508@naver.com>
2022-11-26 17:43:04 +0900

    백준 알고리즘

--- src/programmers_level2/다리를_지나는_트럭.java
+++ src/programmers_level2/다리를_지나는_트럭.java
@@ -41,37 +41,27 @@ public class 다리를_지나는_트럭 {
         }
     }
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> truckOnBridge = new LinkedList<>();
+        int time = 0;
         int bridgeWeight = 0;
-        int count = 0;
+        Queue<Integer> q = new LinkedList<>();
 
         for (int i = 0; i < truck_weights.length; i++) {
-            int truckWeight = truck_weights[i];
-            while(true) {
-                if (truckOnBridge.isEmpty()) {
-                    truckOnBridge.add(truckWeight);
-                    bridgeWeight += truckWeight;
-                    count++;
+            int truck = truck_weights[i];
+            while (true) {
+                if(q.size() == bridge_length) {
+                    bridgeWeight -= q.poll();
+                }
+                if (q.size() < bridge_length && bridgeWeight + truck <= weight) {
+                    q.add(truck);
+                    bridgeWeight += truck;
+                    time++;
                     break;
-                }
-                else if(truckOnBridge.size() == bridge_length) {
-                    Integer poll = truckOnBridge.poll();
-                    bridgeWeight -= poll;
-                }
-                else {
-                    if (bridgeWeight + truckWeight <= weight) {
-                        truckOnBridge.add(truckWeight);
-                        bridgeWeight += truckWeight;
-                        count++;
-                        break;
-                    } else {
-                        truckOnBridge.add(0);
-                        count++;
-                    }
+                } else {
+                    q.add(0);
+                    time++;
                 }
             }
         }
-
-        return count + bridge_length;
+        return time + bridge_length;
     }
 }
